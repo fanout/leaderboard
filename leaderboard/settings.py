89e7c843822c6819dfff4f9146c1831d99ff99e8
Justin Karneges <justin@affinix.com>
2015-05-14 17:40:35 -0700

    debug set by environment

--- leaderboard/settings.py
+++ leaderboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DJANGO_DEBUG', '0') == '1')
 
 ALLOWED_HOSTS = []
 
